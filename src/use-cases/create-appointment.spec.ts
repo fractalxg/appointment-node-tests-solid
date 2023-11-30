7c2a295503fa2c02b2125ce7d44428338df959b5
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-30 16:32:21 -0300

    use case test: create-appointment

---
+++ src/use-cases/create-appointment.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from "vitest"
+import { Appointment } from '../entities/appointment'
+import { CreateAppointment } from "./create-appointment"
+
+describe('Create Appointment', () => {
+    it('should be able to create an appointment', () => {
+        const createAppointment = new CreateAppointment()
+
+        const startsAt = new Date()
+        const endsAt = new Date()
+
+        endsAt.setDate(endsAt.getDate() + 1)
+
+        expect(createAppointment.execute({
+            customer: 'John Doe',
+            startsAt,
+            endsAt
+        })).resolves.toBeInstanceOf(Appointment)
+    })
+})

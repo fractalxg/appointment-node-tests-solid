7d39e5ca73654141d3e30b64be982733f01a6c3a
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-30 21:16:25 -0300

    get future date and test

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -6,7 +6,8 @@ test('create an appointment', () => {
     const startsAt = new Date()
     const endsAt = new Date()
 
-    endsAt.setDate(endsAt.getDate() + 1)
+    startsAt.setDate(startsAt.getDate() + 1)
+    endsAt.setDate(endsAt.getDate() + 2)
 
     const appointment = new Appointment({
         customer: 'John Doe',
@@ -18,11 +19,12 @@ test('create an appointment', () => {
     expect(appointment.customer).toEqual('John Doe')
 })
 
-test('cannot create an appointment with end date before start date', () => {
+test('cannot create an appointment with start date before now', () => {
     const startsAt = new Date()
     const endsAt = new Date()
 
-    endsAt.setDate(endsAt.getDate() - 1)
+    startsAt.setDate(startsAt.getDate() + 2)
+    endsAt.setDate(endsAt.getDate() + 1)
 
     expect(() => {
         return new Appointment({
